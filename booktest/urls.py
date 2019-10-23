9fd4f849232bec5dc262f324d33aa047ed8c93d7
Huxifan <xifan_1995@163.com>
2019-10-23 18:08:04 +0800

    添加登录案例

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,5 +3,9 @@ from booktest import views  # 导入视图views
 
 urlpatterns = [
     url(r'^index$', views.index),  # 首页
-
+    # url(r'^showarg(\d+)$', views.show_args)  # 捕获url参数：位置参数,前面的组的值传给后面视图
+    url(r'^showarg(?P<num>\d+)$', views.show_args),  # 捕获url参数：关键字参数,前面的组的值传给后面视图
+    # 关键字参数，视图中参数名必须和正则表达式组名一致.
+    url(r'^login$', views.login),
+    url(r'^login_check$', views.login_check),  # 用户登录校验
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 # Create your views here.
+# 视图的参数request就是HttpRequest类型的对象
+# request包含浏览器请求的信息
 def index(request):
+    print(request.method)
     return render(request, 'booktest/index.html')
-
-
-from django.shortcuts import render_to_response
 
 
 def page_not_found(request):
@@ -15,3 +17,47 @@ def page_not_found(request):
 
 def page_error(request):
     return render_to_response('500.html')
+
+
+def show_args(request, num):
+    # 数字返回
+    return HttpResponse(num)
+    # 关键字参数，视图中参数名必须和正则表达式组名一致.
+
+
+def login(request):
+    return render(request, 'booktest/login.html')
+
+
+def login_check(request):
+    # 登录校验视图
+    # 1 获取提交的用户名和密码
+    # POST和GET对应提交的方式,都是QueryDict类型的对象
+    print(request.method)
+    print(type(request.POST))  # <class 'django.http.request.QueryDict'>
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')  # 有输出
+    print(username, password)
+    # 2 进行登录的校验。模拟 hu  666
+    if username == 'hu' and password == '666':
+        # 用户名密码正确，跳转首页
+        return redirect('/index')
+    else:
+        # 跳转登录页面
+        return redirect('/login')
+
+    # 3 返回应答
+    # return HttpResponse('OK login_check')
+
+
+'''
+>>> from django.http.request import QueryDict
+>>> QueryDict('a=1&b=2&c=3')
+<QueryDict: {'a': ['1'], 'b': ['2'], 'c': ['3']}>
+>>> q =QueryDict('a=1&b=2&c=3')
+>>> q['a']
+'1'
+>>> q.get('a')
+'1'
+'''
